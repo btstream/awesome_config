471f8d5f9a71d8ecf5027e77ea003e2e41dd9bc6
btstream <btstream@gmail.com>
2020-06-17 18:33:35 +0800

    delete duplicated keybindings

--- config/keybindings.lua
+++ config/keybindings.lua
@@ -152,19 +152,19 @@ awful.keyboard.append_global_keybindings({
         end,
         {description = "restore minimized", group = "client"}
     ),
-    awful.key(
-        {modkey},
-        "x",
-        function()
-            awful.prompt.run {
-                prompt = "Run Lua code: ",
-                textbox = awful.screen.focused().mypromptbox.widget,
-                exe_callback = awful.util.eval,
-                history_path = awful.util.get_cache_dir() .. "/history_eval"
-            }
-        end,
-        {description = "lua execute prompt", group = "awesome"}
-    ),
+    -- awful.key(
+    --     {modkey},
+    --     "x",
+    --     function()
+    --         awful.prompt.run {
+    --             prompt = "Run Lua code: ",
+    --             textbox = awful.screen.focused().mypromptbox.widget,
+    --             exe_callback = awful.util.eval,
+    --             history_path = awful.util.get_cache_dir() .. "/history_eval"
+    --         }
+    --     end,
+    --     {description = "lua execute prompt", group = "awesome"}
+    -- ),
     -- rofi
     awful.key(
         {modkey},
